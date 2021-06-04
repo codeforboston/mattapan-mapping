6dc2b802817b512234ddc3e3a4e747f7824e8e84
Stephen McCall <stephenmccall@Stephens-MacBook-Pro.local>
2021-06-04 17:13:46 -0400

    Initial commit of create-react-app files

---
+++ mattapan-mapping/src/Pages.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+
+export const Home = () => <h1>Home Page</h1>;
+export const Foo = () => <h1>Foo Page</h1>;
+export const Bar = () => <h1>Bar Page</h1>;
---
+++ mattapan-mapping/src/index.tsx
@@ -0,0 +1,30 @@
+
+import * as React from 'react'
+import { HashRouter as Router, Route, Link, Switch } from 'react-router-dom'
+import { render } from 'react-dom';
+import {Home, Foo, Bar} from './Pages';
+import './App.css';
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/foo">Foo</Link>
+            <Link to="/bar">Bar</Link>
+          </nav>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/foo" component={Foo} />
+            <Route exact path="/bar" component={Bar} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
+
+render(<App />, document.getElementById('root'));
