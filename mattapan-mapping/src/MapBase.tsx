faeaea5502937786949b9a1025f43bd1d0d7be86
mcdonough.sh1790@gmail.com <smcdonough@hrmdirect.com>
2021-07-01 19:51:47 -0400

    installed the react mapbox library. Added map to a page and a marker

---
+++ mattapan-mapping/src/MapBase.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+const Map = ReactMapboxGl({
+    accessToken:
+      'pk.eyJ1Ijoic2hheW1tY2Rvbm91Z2giLCJhIjoiY2txbGg1aGVyMDNwdDJ1bzh6NmswMGJqMSJ9.PdmrnSjNMzm37teQx7bAOw'
+  });
+  
+  function MapBase() {
+    return (  
+        <Map
+            //// eslint-disable-next-line
+            style={`mapbox://styles/mapbox/streets-v9`}
+            containerStyle={{
+            height: '100vh',
+            width: '100vw'
+            }}
+        >
+        <Layer type="symbol" id="sm-marker" layout={{ 'icon-image': 'amusement-park-15' }}>
+            <Feature coordinates={[-0.143126, 51.532211]} />
+        </Layer>
+      </Map>
+    );
+  }
+
+  export default MapBase;
--- mattapan-mapping/src/index.tsx
+++ mattapan-mapping/src/index.tsx
@@ -3,8 +3,9 @@ import * as React from 'react'
 import { HashRouter as Router, Route, Link, Switch } from 'react-router-dom'
 import { render } from 'react-dom';
 import {Home, Foo, Bar} from './Pages';
+import MapBase from './MapBase';
 import './App.css';
-
+import './index.css';
 
 class App extends React.Component {
   render() {
@@ -15,11 +16,13 @@ class App extends React.Component {
             <Link to="/">Home</Link>
             <Link to="/foo">Foo</Link>
             <Link to="/bar">Bar</Link>
+            <Link to="/map">Map</Link>
           </nav>
           <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/foo" component={Foo} />
             <Route exact path="/bar" component={Bar} />
+            <Route exact path="/map" component={MapBase} />
           </Switch>
         </div>
       </Router>
