639aee33a36ae844c5aca50c77770fff764da010
brad@smithwebtek.com <brad@smithwebtek.com>
2021-07-02 11:25:35 -0400

    Rearrange index app home

--- mattapan-mapping/src/App.tsx
+++ mattapan-mapping/src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { HashRouter as Router, Route, Link, Switch } from "react-router-dom";
+import "./App.css";
+import { Home, Map } from "./Pages";
+import Legend from "./components/Legend/Legend";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div>
+          <nav>
+            <Link to="/">Home</Link>
+          </nav>
+          <Switch>
+            <Route exact path="/" component={Home} />
+          </Switch>
+          <Map />
+        </div>
+      </Router>
+          <Legend />
     </div>
   );
-}
+};
 
 export default App;
--- mattapan-mapping/src/Pages.tsx
+++ mattapan-mapping/src/Pages.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import HomePage from "./Components/Home/Home";
+import HomePage from "./components/Home/Home";
+import MapPage from "./components/Map/Map";
 
 export const Home = () => <HomePage />;
-export const Foo = () => <h1>Foo Page</h1>;
-export const Bar = () => <h1>Bar Page</h1>;
+export const Map = () => <MapPage />;
