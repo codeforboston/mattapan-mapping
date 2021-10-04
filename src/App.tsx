96658ae6fbd8df008eb8e94ab338b465a6c88362
dickson <kr.d.tsl@gmail.com>
2021-10-04 19:51:37 -0400

    mapboxd

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import './App.css';
 import Header from './organisms/Header';
 import { GeneralIntro } from './organisms/GeneralIntro';
-import Map from './organisms/Map';
+import { Map, Map2 } from './organisms/Map';
 import Footer from './organisms/Footer';
 import { PersonaCardSet } from './organisms/PersonaCardSet';
 import { Theme } from './theme/Theme';
@@ -16,6 +16,7 @@ export default function App() {
       <PersonaCardSet />
       <GeneralIntro />
       <Map />
+      <Map2 />
       {/* <StatBanner2 /> */}
       {/* <CardList /> */}
       <Footer />
