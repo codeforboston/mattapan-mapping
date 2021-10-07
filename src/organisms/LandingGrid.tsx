a907189dfbb615b4f8b90fdfea404a3ec8f13176
dickson <kr.d.tsl@gmail.com>
2021-10-07 19:44:28 -0400

    comment out weird double hover info tile

--- src/organisms/LandingGrid.tsx
+++ src/organisms/LandingGrid.tsx
@@ -20,10 +20,14 @@ const CardContainer = styled.div<{ color: string }>`
   width: 100%;
   height: 500px;
   position: relative;
+  overflow: hidden;
 
   &:hover {
     .card-image {
       transform: scale(1);
+    }
+    .card-info {
+      transform: translateY(-100%);
     }
   }
 `;
@@ -43,17 +47,37 @@ const CardTitle = styled.span`
   color: white;
   transform: translateY(-50%);
   top: 50%;
+  left: 8px;
   font-size: 60px;
   font-family: ${ props => props.theme.fonts.badscript };
   z-index: 4;
   text-shadow: 0 0 2px black;
 `;
 
-const LandingCard = ( { children, color, src } : { children: string; color: string; src: string }) => {
+const CardInfo = styled.div`
+  height: 50%;
+  width: 100%;
+  transform: translateY(150%);
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #F2F2F2;
+  transition: transform ease-out 0.6s;
+`;
+
+const InfoText = styled.div`
+  color: black;
+  font-family: ${ props => props.theme.fonts.dm };
+  font-size: ${ props => props.theme.fonts.fontSize.regular };
+`;
+
+const LandingCard = ( { children, color, src, title } : { children: string; color: string; src: string; title: string }) => {
   return (
     <CardContainer color={ color }>
-      <CardTitle>{ children }</CardTitle> 
+      <CardTitle>{ title }</CardTitle> 
       <CardImage className='card-image' src={ src } />
+      {/* <CardInfo className='card-info'><InfoText>{ children }</InfoText></CardInfo> */}
     </CardContainer>
   )
 }
@@ -61,12 +85,12 @@ const LandingCard = ( { children, color, src } : { children: string; color: stri
 export const LandingGrid = () => {
   return (
     <LandingGridContainer>
-      <LandingCard color={ Theme.colors.blue } src='https://miro.medium.com/max/1838/1*BC6WfcrUboUP3MK_fP_0uw.jpeg'>About</LandingCard>
-      <LandingCard color={ Theme.colors.peach } src='https://upload.wikimedia.org/wikipedia/commons/8/8d/Frog_on_palm_frond.jpg'>Explore</LandingCard>
-      <LandingCard color={ Theme.colors.teal } src='https://media.wired.com/photos/5cdefb92b86e041493d389df/191:100/w_1280,c_limit/Culture-Grumpy-Cat-487386121.jpg'>Meet</LandingCard>
-      <LandingCard color={ Theme.colors.orange } src='https://tinyurl.com/uvc9yter'>Share</LandingCard>
-      <LandingCard color={ Theme.colors.red } src='http://cdn.cnn.com/cnnnext/dam/assets/210715140819-05-modernist-pizza-travel-restricted.jpg'>Get Invovled</LandingCard>
-      <LandingCard color={ Theme.colors.brown } src='https://nickpassey.com/wp-content/uploads/2016/10/Featured.jpg'>Contact Us?</LandingCard>
+      <LandingCard title='About' color={ Theme.colors.blue } src='https://miro.medium.com/max/1838/1*BC6WfcrUboUP3MK_fP_0uw.jpeg'>More info on About</LandingCard>
+      <LandingCard title='Explore' color={ Theme.colors.peach } src='https://upload.wikimedia.org/wikipedia/commons/8/8d/Frog_on_palm_frond.jpg'>More info on Explore</LandingCard>
+      <LandingCard title='Meet' color={ Theme.colors.teal } src='https://media.wired.com/photos/5cdefb92b86e041493d389df/191:100/w_1280,c_limit/Culture-Grumpy-Cat-487386121.jpg'>Meet</LandingCard>
+      <LandingCard title='Share' color={ Theme.colors.orange } src='https://tinyurl.com/uvc9yter'>More info on Share</LandingCard>
+      <LandingCard title='Get Invovled' color={ Theme.colors.red } src='http://cdn.cnn.com/cnnnext/dam/assets/210715140819-05-modernist-pizza-travel-restricted.jpg'>More info on Get Invovled</LandingCard>
+      <LandingCard title='Contact Us?' color={ Theme.colors.brown } src='https://nickpassey.com/wp-content/uploads/2016/10/Featured.jpg'>More info on Contact Us?</LandingCard>
     </LandingGridContainer>
   )
 }
