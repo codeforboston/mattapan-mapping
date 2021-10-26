8d7f1baf4e9ef10e48ec544e1ac87e00859e6fcb
dickson <kr.d.tsl@gmail.com>
2021-10-26 19:51:15 -0400

    Add more pop for video

--- src/organisms/SplashVideo.tsx
+++ src/organisms/SplashVideo.tsx
@@ -2,8 +2,10 @@ import styled from '@emotion/styled';
 
 const VideoContainer = styled.video`
     height: 100%;
-    width: 100%;
+    width: 80%;
     z-index: 1;
+    box-shadow: 0 0 10px 0 black;
+    border-radius: 4px;
 `;
 
 
@@ -16,4 +18,11 @@ export const SplashVideo = () => (
         playsInline
         src='https://assets.mixkit.co/videos/preview/mixkit-daytime-city-traffic-aerial-view-56-large.mp4'
     />
-)
+);
+
+export const SplashVideoContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    height: 100%;
+`;
