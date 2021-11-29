c26606f91eef374dc4b6333effd794b536379858
dickson <kr.d.tsl@gmail.com>
2021-11-29 18:29:12 -0500

    convert to async/await

--- src/pages/ExplorePage.tsx
+++ src/pages/ExplorePage.tsx
@@ -1,51 +1,35 @@
 import * as React from 'react';
-// import fetchAll from '@/utils/fetchAll';
 import { ExplorePageMap } from '@/organisms/PageMap';
-import { EXPLORE_PAGE_DATA_ENUM } from '@/data/ExplorePageData';
-
-// const getExplorePageData = async () => {
-//   const boundaryDataFiles = Object.values(EXPLORE_PAGE_DATA_ENUM).map(boundary => boundary.data);
-//   const boundaryIds = Object.keys(EXPLORE_PAGE_DATA_ENUM);
-//   const data: GeoJson[] = await fetchAll(boundaryDataFiles);
-
-//   const ExplorePageData = boundaryIds.map((id: string, idx) => {
-//     return {
-//       data: data[idx],
-//       name: data[idx]['name'],
-//       id: id,
-//       type: 'line',
-//       color: EXPLORE_PAGE_DATA_ENUM[id].color,
-//       visible: EXPLORE_PAGE_DATA_ENUM[id].visible,
-//     }
-//   });
-
-//   return ExplorePageData
-// }
+import { EXPLORE_PAGE_BOUNDARY_DATA } from '@/data/ExplorePageData';
 
 export default function ExplorePage() {
-  const urls = Object.values(EXPLORE_PAGE_DATA_ENUM).map(boundary => boundary.data);
-  const boundaryIds = Object.keys(EXPLORE_PAGE_DATA_ENUM);
+  const urls = EXPLORE_PAGE_BOUNDARY_DATA.map(boundary => boundary.file);
+  const boundaryIds = EXPLORE_PAGE_BOUNDARY_DATA.map(boundary => boundary.id)
+  const boundaryDataFileLocations = urls.map(url => fetch(url).then(res => res.json()));
   const [data, setData]: [MapGeoJsonData[], React.Dispatch<React.SetStateAction<any>>] = React.useState([]);
 
   React.useEffect(() => {
-    const boundaryData = urls.map(url => 
-      fetch(url).then(res => res.json())  
-    );
-    Promise.all(boundaryData).then(data => {
-      const ExplorePageData = boundaryIds.map((id: string, idx) => {
-        return {
-          data: data[idx],
-          name: data[idx]['name'],
-          id: id,
-          type: 'line',
-          color: EXPLORE_PAGE_DATA_ENUM[id].color,
-          visible: EXPLORE_PAGE_DATA_ENUM[id].visible,
-        }
-      });
-      
-      setData(ExplorePageData)
-    })
-  }, [boundaryIds, urls]);
+    const fetchAll = async () => {
+      try {
+        const rawData = await Promise.all(boundaryDataFileLocations);
+        const ExplorePageData = boundaryIds.map((id: string, idx) => {
+          return {
+            data: rawData[idx],
+            name: rawData[idx]['name'],
+            id: id,
+            type: 'line',
+            color: EXPLORE_PAGE_BOUNDARY_DATA[idx].color,
+            visible: EXPLORE_PAGE_BOUNDARY_DATA[idx].visible,
+          }
+        })
+        setData(ExplorePageData)
+      } catch (err) {
+          console.log('error')
+      }
+    }
+
+    fetchAll();
+  }, [boundaryIds, boundaryDataFileLocations]);
 
   return (
     <ExplorePageMap data={ data } />
