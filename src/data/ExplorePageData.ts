73c4f0ba8d5c805e05300bc310f9e37b6227f891
dickson <kr.d.tsl@gmail.com>
2021-11-29 18:29:51 -0500

    add boundaries

--- src/data/ExplorePageData.ts
+++ src/data/ExplorePageData.ts
@@ -1,42 +1,94 @@
 import { Theme } from '@/theme/Theme';
-import { GreaterMattapanZoning as GreaterMattapanZoningData } from '@/data/GreaterMattapanZoning';
-import { BlueHillAveCorridor as BlueHillAveCorridorData } from '@/data/BlueHillAveCorridor';
 
-export const EXPLORE_PAGE_DATA_ENUM: InterimData = {
-  blueHillAveCorridor: {
-    data: './boundaries/Blue_Hill_Ave_Corridor.json',
-    color: Theme.colors.blue,
-    visible: true,
-  },
-  greaterMattapanZoning: {
-    data: './boundaries/greater_mattapan_zoning.json',
-    color: Theme.colors.orange,
-    visible: true,
-  }
-}
-
-export const ExplorePageData: MapGeoJsonData[] = [
+export const EXPLORE_PAGE_BOUNDARY_DATA: GeoJsonDetails[] = [
   {
-    data: BlueHillAveCorridorData,
     id: 'blueHillAveCorridor',
-    name: 'Blue Hill Ave Corridor',
-    type: 'line',
+    file: './boundaries/Blue_Hill_Ave_Corridor.json',
+    color: Theme.colors.blue,
+    visible: false,
+  },
+  {
+    id: 'greaterMattapanZoning',
+    file: './boundaries/greater_mattapan_zoning.json',
+    color: Theme.colors.orange,
+    visible: true,
+  },
+  {
+    id: 'zoningSubdistricts',
+    file: './boundaries/1Zoning_Subdistricts.geojson',
+    color: Theme.colors.blue,
+    visible: false,
+  },
+  {
+    id: 'cummimnsHwyCorridor',
+    file: './boundaries/Cummins_Hwy_Corridor.json',
+    color: Theme.colors.orange,
+    visible: false,
+  },
+  {
+    id: 'dotHpZoningSubdistricts',
+    file: './boundaries/Dot_HP_Zoning_Subdistricts.json',
     color: Theme.colors.blue,
     visible: true,
   },
   {
-    data: GreaterMattapanZoningData,
-    id: 'greaterMattapanZoning',
-    name: 'Greater Mattapan Zoning',
-    type: 'line',
+    id: 'greaterMattapanMergedPoly',
+    file: './boundaries/Greater_Mattapan_Merged_Poly.json',
+    color: Theme.colors.blue,
+    visible: true,
+  },
+  {
+    id: 'greaterMattapanNeighborhoodLine',
+    file: './boundaries/Greater_Mattapan_Neighborhood_Line.ojson',
+    color: Theme.colors.blue,
+    visible: true,
+  },
+  {
+    id: 'mattapanNeighborhood',
+    file: './boundaries/mattapan_neighborhood.json',
+    color: Theme.colors.blue,
+    visible: true,
+  },
+  {
+    id: 'mattapanSquare',
+    file: './boundaries/Mattapan_Square_qtr_mile.json',
+    color: Theme.colors.blue,
+    visible: true,
+  },
+  {
+    id: 'mattapanZoningSubdistricts',
+    file: './boundaries/Mattapan_Zoning_subdistricts.geojson',
+    color: Theme.colors.blue,
+    visible: true,
+  },
+  {
+    id: 'mattapanPlanningDistrict',
+    file: './boundaries/mattapan_planning_district.json',
+    color: Theme.colors.blue,
+    visible: true,
+  },
+  {
+    id: 'mortonStCorridor',
+    file: './boundaries/Morton_St_Corridor.json',
+    color: Theme.colors.blue,
+    visible: true,
+  },
+  {
+    id: 'mattapanPlanBoundary',
+    file: './boundaries/PLAN_Mattapan_Poly.json',
+    color: Theme.colors.blue,
+    visible: true,
+  },
+  {
+    id: 'riverStCorridor',
+    file: './boundaries/River_St_Corridor_FeaturesTo.json',
     color: Theme.colors.orange,
     visible: true,
   },
-];
-
-
-export const ExplorePageToggleStates: () => FeatureToggleState = () => {
-  return ExplorePageData.reduce(
-    (obj: any, item: any) => Object.assign(obj,{ [item.id]: item.visible }
-  ), {});
-};
+  {
+    id: 'subDistricts',
+    file: './boundaries/sd.json',
+    color: Theme.colors.blue,
+    visible: true,
+  },
+]
