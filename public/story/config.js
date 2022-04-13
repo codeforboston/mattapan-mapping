4b2af44ed6778e7b5ae776023d4eda5544eb8ff0
Thad Kerosky <contact@thadk.net>
2022-04-12 21:12:19 -0400

    demo demo demo

--- public/story/config.js
+++ public/story/config.js
@@ -6,7 +6,7 @@ var config = {
     //projection: 'equirectangular',
     //Read more about available projections here
     //https://docs.mapbox.com/mapbox-gl-js/example/projections/
-    inset: true,
+    inset: false,
     theme: 'dark',
     use3dTerrain: false, //set true for enabling 3D maps.
     title: 'The Title Text of this Story',
